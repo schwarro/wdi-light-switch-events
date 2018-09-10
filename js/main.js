ef29323b0250a7894ef0d2948d302410827e1fa2
schwarro <rob.ap.schwartz@gmail.com>
2018-09-10 13:41:23 -0400

    completed exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(e){
+  var status = document.querySelector(".status");
+  var body = document.querySelector("body");
+  var button = document.querySelector(".switch.on");
 
+  button.addEventListener("click", switchModes, false);
 
+  function switchModes() {
+    this.classList.toggle("on");
+    this.classList.toggle("off");
+    body.classList.toggle("light");
+    body.classList.toggle("dark");
+
+    if (button.classList.contains("on")) {
+      status.innerHTML = "It's so bright in here!";
+    } else if (button.classList.contains("off")) {
+      status.innerHTML = "It's so dark in here!";
+    }
+  }
+});
